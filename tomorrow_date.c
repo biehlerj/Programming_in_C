610946d8b0cbc1a2ae8d9d1f6a0b3759309d0bf5
Jacob Biehler <biehlerj@gmail.com>
2017-10-16 16:56:53 -0700

    Commit to line up if statements

--- tomorrow_date.c
+++ tomorrow_date.c
@@ -20,24 +20,24 @@ int main(void)
     printf("Enter today's date (mm dd yyyy): ");
     scanf("%i%i%i", &today.month, &today.day, &today.year);
 
-    if (today.day != numberOfDays(today))
-      {
-	tomorrow.day = today.day + 1;
-	tomorrow.month = today.month;
-	tomorrow.year = today.year;
-      }
+    if (today.day != numberOfDays (today))
+    {
+	     tomorrow.day = today.day + 1;
+	     tomorrow.month = today.month;
+	     tomorrow.year = today.year;
+    }
     else if (today.month == 12) /* end of year */
-      {
-	tomorrow.day = 1;
-	tomorrow.month = 1;
-	tomorrow.year = today.year + 1;
-      }
+    {
+	     tomorrow.day = 1;
+	     tomorrow.month = 1;
+	     tomorrow.year = today.year + 1;
+    }
     else                        /* end of month */
-      {
-	tomorrow.day = 1;
-	tomorrow.month = today.month + 1;
-	tomorrow.year = today.year;
-      }
+    {
+	     tomorrow.day = 1;
+	     tomorrow.month = today.month + 1;
+	     tomorrow.year = today.year;
+    }
     printf("Tomorrow's date is %i/%i/%.2i.\n", tomorrow.month, tomorrow.day, tomorrow.year % 100);
     return (0);
 }
@@ -49,11 +49,13 @@ int numberOfDays(struct date d)
   int days;
   boo1 isLeapYear(struct date d);
   const int daysPerMonth[12] =
-  { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+    { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
   if (isLeapYear(d) == true && d.month == 2)
     days = 29;
   else
     days = daysPerMonth[d.month - 1];
+
   return (days);
 }
 
